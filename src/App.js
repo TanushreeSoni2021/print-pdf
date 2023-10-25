da5f35f2e292864d1ad207a743a19fb341b9e321
Tanushree Soni <108271833+TanushreeSoni2021@users.noreply.github.com>
2023-10-25 13:33:29 +0530

    we can print our pdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PDFViewer } from "@react-pdf/renderer";
+import MyDocument from "./MyDocument";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <PDFViewer width="800" height="600">
+        <MyDocument />
+      </PDFViewer>
     </div>
   );
 }
---
+++ src/MyDocument.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#dadada"
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+        <View style={styles.listItem}>
+          <Text>Item 1</Text>
+        </View>
+        <View style={styles.listItem}>
+          <Text>Item 2</Text>
+        </View>
+        <View style={styles.listItem}>
+          <Text>Item 3</Text>
+        </View>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+export default MyDocument;
